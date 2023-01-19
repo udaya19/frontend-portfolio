180ad9bc45f3fecee4279e03589463058da082bf
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-19 21:47:55 +0530

    About component api fetching

---
+++ src/api/about.js
@@ -0,0 +1,3 @@
+import apiInstance from ".";
+
+export const getAboutApi = () => apiInstance.get("/about");
--- src/components/UserComponents/About.jsx
+++ src/components/UserComponents/About.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { getUserImageApi } from "../../api/userApi";
+import { getAboutApi } from "../../api/about";
 const About = () => {
   const [imageUrl, setImageUrl] = useState();
+  const [about, setAbout] = useState([]);
   useEffect(() => {
     getImage();
+    getAbout();
   }, []);
   const getImage = async () => {
     const { success, userImg } = await (await getUserImageApi()).data;
@@ -12,24 +15,21 @@ const About = () => {
       setImageUrl(imgUrl);
     }
   };
+  const getAbout = async () => {
+    const { results } = await (await getAboutApi()).data;
+    setAbout(results);
+  };
   return (
     <div className="p-7 ">
       <div className="about-image">
         <img src={imageUrl} alt="" className="" />
       </div>
       <div className="aboutMe mt-7">
-        <p className="w-2/3 text-mediumGray font-semibold text-lg ml-60">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia vero
-          dicta error dignissimos ad ullam, accusamus voluptas id. Eos nihil est
-          sint enim veniam laboriosam possimus maiores culpa earum ipsam. Lorem,
-          ipsum dolor sit amet consectetur adipisicing elit. Rem nostrum,
-          corrupti quibusdam fuga quos ab laboriosam a perferendis voluptatem,
-          impedit eos delectus odit eum sequi error! Quae dolores recusandae
-          nemo? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Dolorum, assumenda officia eius fugiat minima voluptatibus expedita? A
-          temporibus fugit porro quisquam itaque necessitatibus id perferendis
-          sit, dicta earum similique illum?
-        </p>
+        {about.map((a) => (
+          <p className="w-2/3 text-mediumGray font-semibold text-lg ml-60">
+            {a.content}
+          </p>
+        ))}
       </div>
     </div>
   );
