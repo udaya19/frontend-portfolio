e638266c71080769af9188974ef091424034e453
Gonuguntla Udaya Kiran <102300201+udaya19@users.noreply.github.com>
2023-01-27 17:00:20 +0530

    fetching data from projects API

---
+++ src/api/projectApi.js
@@ -0,0 +1,3 @@
+import apiInstance from ".";
+
+export const getProjectApi = () => apiInstance.get("/project");
--- src/components/UserComponents/Projects.jsx
+++ src/components/UserComponents/Projects.jsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import project from "../../images/projects.png";
+import { getProjectApi } from "../../api/projectApi";
 import Title from "../Title";
 
 const Projects = () => {
+  const [projects, setProjects] = useState([]);
+  const getProjects = async () => {
+    try {
+      const { success, results } = await (await getProjectApi()).data;
+      if (success) {
+        setProjects(results);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    getProjects();
+  }, []);
   return (
     <>
       <Title iconName="work" title="Projects" />
-      <div className="project-container flex flex-wrap gap-10">
-        <div className="flex w-1/2">
-          <img src={project} alt="" width="300px" height="300px" />
-          <div>
-            <a href="/">
-              <p className="mt-10 font-bold text-primary text-[20px]">
-                Project Topic
-              </p>
-              <p className="text-mediumGray">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-                delectus quidem placeat quisquam facere nihil, sunt deserunt
-                praesentium dolore similique. Ea architecto neque repellat saepe
-                laboriosam quasi beatae laborum recusandae.
-              </p>
-            </a>
+      {projects.map((p) => (
+        <div className="project-container flex flex-wrap gap-10">
+          <div className="flex w-1/2">
+            <img src={project} alt="" width="300px" height="300px" />
+            <div>
+              <a href={p.link}>
+                <p className="mt-10 font-bold text-primary text-[20px]">
+                  {p.title}
+                </p>
+                <p className="text-mediumGray">{p.description}</p>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </>
   );
 };
